0c1dd924c724890e25890f3adba218fbd8c827e2
imran <sheikhimran210903@gmail.com>
2024-06-01 06:06:41 -0700

    if else

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const student = {
+    name: "imran sheikh",
+    age: 25,
+    roll: 2
+  }
+  console.log(student, "student");
+
+  const student2 = {
+    name: "muskan kumari",
+    age: 25,
+    roll: 5
+  }
+
+  if(student.age === student2.age){
+    console.log("Friends");
+  }else{
+    console.log("NOt friends")
+  }
+
+  
   return (
     <div className="App">
       <header className="App-header">
